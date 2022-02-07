c3e085608c9cfeeb4f74079c85a2bc8ed38dbfb1
MinhNguyen <minhnguyen181199@gmail.com>
2022-02-07 09:39:12 +0200

    Set up firebase and angular material for project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'fb-clone-7ceda',
+    appId: '1:335288930120:web:a59c1cbe782a8b86f9ed7d',
+    storageBucket: 'fb-clone-7ceda.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDR5pItFQfaelUNL_pnM3eSaatBNAITZ_U',
+    authDomain: 'fb-clone-7ceda.firebaseapp.com',
+    messagingSenderId: '335288930120',
+  },
   production: true
 };
